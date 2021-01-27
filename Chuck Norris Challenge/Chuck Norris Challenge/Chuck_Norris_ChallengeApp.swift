a457234e0d17235903db80057240dd39b91f69f8
Bruno <brunoluebke@gmail.com>
2021-01-27 18:12:39 -0300

    Created Initial Project
    Created the xcode project with SwiftUI template.

---
+++ Chuck Norris Challenge/Chuck Norris Challenge/Chuck_Norris_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chuck_Norris_ChallengeApp.swift
+//  Chuck Norris Challenge
+//
+//  Created by Bruno Luebke on 27/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Chuck_Norris_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
